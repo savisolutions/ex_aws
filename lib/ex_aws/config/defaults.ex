be660f0fef46ea423e43a0a6358a198880d64dae
keatz55 <keatz55@gmail.com>
2021-05-04 17:08:12 -0600

    updates regex

--- lib/ex_aws/config/defaults.ex
+++ lib/ex_aws/config/defaults.ex
@@ -77,7 +77,7 @@ defmodule ExAws.Config.Defaults do
   end
 
   @partitions [
-    {~r/^(us|eu|ap|sa|ca)\-\w+\-(?!000$)\d+$/, "aws"},
+    {~r/^(us|eu|ap|sa|ca)\-\w+\-(?!00[0-9]$)\d+$/, "aws"},
     {~r/^cn\-\w+\-\d+$/, "aws-cn"},
     {~r/^us\-gov\-\w+\-\d+$/, "aws-us-gov"}
   ]
